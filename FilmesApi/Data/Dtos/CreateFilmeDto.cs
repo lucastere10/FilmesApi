911cc7c571e43023c1bd1b7e73bc79486b88af17
Lucas Caldas <lucasmedeiroscaldas@yahoo.com.br>
2024-04-29 18:39:20 -0300

    feat: controller e documentação de filmes

---
+++ FilmesApi/Data/Dtos/CreateFilmeDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Data.Dtos;
+
+public class CreateFilmeDto
+{
+    [Required(ErrorMessage = "O Filme é obrigatório")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O Gênero é obrigatório")]
+    [StringLength(50)]
+    public string Genero { get; set; }
+    [Required]
+    [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 e 600")]
+    public int Duracao { get; set; }
+}
---
+++ FilmesApi/Models/Filme.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
+
+public class Filme
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "O Titulo é obrigatório")]
+    public string Titulo { get; set; }
+    [Required(ErrorMessage = "O Gênero é obrigatório")]
+    [MaxLength(100)]
+    public string Genero { get; set; }
+    [Required]
+    [Range(70,600, ErrorMessage = "A duração deve ter entre 70 e 600")]
+    public int Duracao { get; set; }
+}
