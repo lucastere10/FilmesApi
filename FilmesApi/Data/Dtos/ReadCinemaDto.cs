c8fe54b447413509bb3be8ca3154ef3815bc15da
Lucas Caldas <lucasmedeiroscaldas@yahoo.com.br>
2024-04-30 16:48:44 -0300

    feature: entidades cinema e endereco
    add: relacionamento de entidades
    add: migrations no banco de dados

---
+++ FilmesApi/Data/Dtos/ReadCinemaDto.cs
@@ -0,0 +1,12 @@
+﻿using FilmesApi.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Data.Dtos;
+
+public class ReadCinemaDto
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public ReadEnderecoDto Endereco { get; set; }
+    public ICollection<Sessao> Sessoes { get; set; }
+}
---
+++ FilmesApi/Profiles/EnderecoProfiler.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using FilmesApi.Data.Dtos;
+using FilmesApi.Models;
+
+namespace FilmesApi.Profiles;
+
+public class EnderecoProfiler : Profile
+{
+    public EnderecoProfiler()
+    {
+        CreateMap<CreateEnderecoDto, Endereco>();
+        CreateMap<Endereco, ReadEnderecoDto>();
+        CreateMap<UpdateEnderecoDto, Endereco>();
+    }
+}
